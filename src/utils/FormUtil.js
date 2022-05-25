3216935222ef3257a8e31e061f184efee8714c3f
ShubhamBajaj <shmbajaj32@gmail.com>
2022-05-25 13:31:28 +0530

    Utils For Auth Form and Auth Slice

---
+++ src/utils/FormUtil.js
@@ -0,0 +1,57 @@
+import { toast } from "react-toastify";
+
+const passwordInputInfo = {
+  icon: "visibility",
+  type: "password",
+};
+
+function authStatusAction(
+  status,
+  message,
+  type,
+  setFormButtonTextState,
+  setFormErrorMessageState
+) {
+  if (status === "loading") {
+    setFormButtonTextState("Loading...");
+  }
+  if (status === "failed") {
+    setFormButtonTextState(type);
+    setFormErrorMessageState(message);
+  }
+  if (status === "success") {
+    setFormButtonTextState(type);
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+}
+
+function togglePasswordInputInfo(state, setState) {
+  if (state.icon === "visibility") {
+    setState((p) => ({ ...p, icon: "visibility_off", type: "text" }));
+  } else {
+    setState((p) => ({ ...p, icon: "visibility", type: "password" }));
+  }
+}
+
+function fillGuestLoginCredentials(setState) {
+  setState((p) => ({
+    ...p,
+    email: "guestemail@login.com",
+    password: "12345678",
+  }));
+}
+
+export {
+  togglePasswordInputInfo,
+  passwordInputInfo,
+  authStatusAction,
+  fillGuestLoginCredentials,
+};
