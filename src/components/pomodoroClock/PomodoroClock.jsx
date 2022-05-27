1430e68ce75933b4ad77718c26140f14d567b2f7
ShubhamBajaj <shmbajaj32@gmail.com>
2022-05-27 14:00:13 +0530

    Pomodoro Page Finished, Along With PomodoroClock

---
+++ src/components/pomodoroClock/PomodoroClock.jsx
@@ -0,0 +1,78 @@
+import "./PomodoroClock.styles.css";
+import { useEffect, useState, useRef } from "react";
+import { computeClockProgress } from "./PomodoroClock.helpers";
+
+function PomodoroClock({ time, info, setTaskIsDone, timerIdRef }) {
+  const TOTAL_TIME = time * 60;
+  const PROGRESS_END_VALUE = TOTAL_TIME;
+  const SPEED = 1000;
+
+  const progressValueRef = useRef(time * 60);
+  const [progressValue, setProgressValue] = useState(time * 60);
+
+  useEffect(() => {
+    if (progressValueRef.current === 0) {
+      clearInterval(timerIdRef.current);
+      timerIdRef.current = null;
+      setTaskIsDone();
+    }
+  }, [progressValueRef.current]);
+
+  useEffect(() => {
+    if (info.start && !info.done && !timerIdRef.current) {
+      startTimer();
+    } else if (info.pause && timerIdRef.current) {
+      pauseTimer();
+    } else if (info.restart) {
+      restartTimer();
+    }
+  }, [info]);
+
+  // * Here introducing modularity brings, passing 2-4 arguments to each function
+  function startTimer() {
+    let timerId = setInterval(() => {
+      progressValueRef.current--;
+      setProgressValue(progressValueRef.current);
+    }, SPEED);
+    timerIdRef.current = timerId;
+  }
+
+  function pauseTimer() {
+    clearInterval(timerIdRef.current);
+    timerIdRef.current = null;
+  }
+
+  function restartTimer() {
+    pauseTimer();
+    progressValueRef.current = TOTAL_TIME;
+    setProgressValue(progressValueRef.current);
+    startTimer();
+  }
+
+  // * Constantly updating for pomodoro clock
+  let { clockMainColor, percentage, minutes, seconds } = computeClockProgress(
+    progressValue,
+    PROGRESS_END_VALUE
+  );
+
+  return (
+    <div
+      className="circular-progress"
+      style={{
+        background: `conic-gradient(
+          ${clockMainColor} ${percentage * 3.6}deg,
+          #cadcff ${percentage * 3.6}deg
+      )`,
+      }}
+    >
+      <div className="value-container">
+        <div>
+          {minutes}m : {seconds}s
+        </div>
+        <div>Out of {time} min</div>
+      </div>
+    </div>
+  );
+}
+
+export { PomodoroClock };
---
+++ src/pages/pomodoro/Pomodoro.jsx
@@ -0,0 +1,165 @@
+import "./Pomodoro.styles.css";
+import { PomodoroClock } from "components";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Toast } from "utils";
+import { getTask, postTask } from "features";
+
+function Pomodoro() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [taskInfo, setTaskInfo] = useState(null);
+  const [pomodoroInfo, setPomodoroInfo] = useState({
+    start: false,
+    pause: false,
+    restart: false,
+    done: false,
+  });
+  const uid = useSelector((state) => state.auth.uid);
+  const { task, status, message } = useSelector((state) => state.tasks);
+  const timerIdRef = useRef(null);
+  const dispatch = useDispatch();
+
+  function setTaskIsDone() {
+    setPomodoroInfo((p) =>
+      Object.assign({}, p, {
+        start: false,
+        pause: false,
+        restart: false,
+        done: true,
+      })
+    );
+  }
+
+  useEffect(() => {
+    dispatch(getTask({ uid, taskId: location.pathname.split("/")[2] }));
+  }, []);
+
+  useEffect(() => {
+    if (status === "failed") {
+      Toast.warning(message);
+    }
+    if (status === "success" && task?.Title) {
+      setTaskInfo((p) => Object.assign({}, p, task));
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (pomodoroInfo.done) {
+      dispatch(
+        postTask({
+          prevTaskInfo: taskInfo,
+          taskInfo: { ...taskInfo, IsDone: true },
+          uid,
+        })
+      );
+    }
+    // * Reset task status to not done
+    else if (!pomodoroInfo.done && pomodoroInfo.restart) {
+      dispatch(
+        postTask({
+          prevTaskInfo: taskInfo,
+          taskInfo: { ...taskInfo, IsDone: false },
+          uid,
+        })
+      );
+    }
+  }, [pomodoroInfo]);
+
+  return (
+    <main className="main pomodoro-main">
+      {taskInfo && (
+        <section className="pomodoro-header">
+          <button className="return-cta" onClick={() => navigate("/tasks")}>
+            <span className="material-icons-outlined return-cta-icon">
+              arrow_back
+            </span>
+            <span className="return-cta-text">Return To Tasks</span>
+          </button>
+        </section>
+      )}
+      {taskInfo && (
+        <section className="pomodoro-body">
+          <section className="pomodoro-actions">
+            <PomodoroClock
+              time={taskInfo.Time}
+              info={pomodoroInfo}
+              setTaskIsDone={setTaskIsDone}
+              timerIdRef={timerIdRef}
+            />
+            <div className="pomodoro-actions-cta">
+              <button
+                className="cta-start"
+                onClick={() =>
+                  setPomodoroInfo((p) => ({
+                    ...p,
+                    start: true,
+                    pause: false,
+                    restart: false,
+                  }))
+                }
+                disabled={
+                  pomodoroInfo.done ||
+                  pomodoroInfo.restart ||
+                  pomodoroInfo.start
+                }
+              >
+                <span className="material-icons-outlined">play_arrow</span>
+                <span>{pomodoroInfo.pause ? "Resume" : "Start"}</span>
+              </button>
+              <button
+                className="cta-pause"
+                onClick={() => {
+                  if (timerIdRef.current) {
+                    setPomodoroInfo((p) => ({
+                      ...p,
+                      pause: true,
+                      start: false,
+                      restart: false,
+                    }));
+                  }
+                }}
+                disabled={pomodoroInfo.done || pomodoroInfo.pause}
+              >
+                <span className="material-icons-outlined">pause</span>
+                <span>Pause</span>
+              </button>
+              <button
+                className="cta-restart"
+                onClick={() =>
+                  setPomodoroInfo((p) => ({
+                    ...p,
+                    start: false,
+                    pause: false,
+                    restart: true,
+                    done: false,
+                  }))
+                }
+              >
+                <span className="material-icons-outlined">restart_alt</span>
+                <span>Restart</span>
+              </button>
+            </div>
+          </section>
+          <section className="pomodoro-description">
+            <h1 className="pomodoro-title">{taskInfo.Title}</h1>
+            <p className="pomodoro-description-text">{taskInfo.Description}</p>
+            <div className="pomodoro-tags">
+              <span>Tag :</span>
+              <div className="tasks-tags">
+                <span className="tag">{taskInfo.Tag}</span>
+              </div>
+            </div>
+            <span>Date Added: {taskInfo.DateAdded}</span>
+          </section>
+        </section>
+      )}
+      {status === "loading" && !taskInfo && (
+        <h1 className="pomodoro-body">Loading...</h1>
+      )}
+    </main>
+  );
+}
+
+export { Pomodoro };
